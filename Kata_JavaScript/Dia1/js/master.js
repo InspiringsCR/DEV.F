612de4c8e308a4c7b9fe4aff621c287ba1fbe0ba
Gabriel Granados Ureña <gabrig1404@gmail.com>
2022-08-08 20:50:54 -0600

    Dia 1

---
+++ Kata_JavaScript/Dia1/js/master.js
@@ -0,0 +1,20 @@
+// const edad = 25;
+
+// function saludar(){
+//     console.log(edad);
+//     let nombre = 'Migue';
+//     console.log(nombre);
+// }
+
+// function saludar2(){
+//     console.log(edad);
+// }
+
+// saludar();
+// saludar2();
+
+const nombre = 'Jose';
+const pais = 'Mexico'
+const generacion = 19
+
+console.log('Bienvenido ', nombre, 'de', pais, 'de la generacion', generacion);
