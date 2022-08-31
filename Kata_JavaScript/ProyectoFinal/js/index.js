0931baa37d7cd2a8b86f6d8d1446775791f578ca
Gabriel Granados Ureña <gabrig1404@gmail.com>
2022-08-30 19:49:14 -0600

    Update data

--- Kata_JavaScript/ProyectoFinal/js/index.js
+++ Kata_JavaScript/ProyectoFinal/js/index.js
@@ -6,80 +6,34 @@ const errorIdField = document.getElementById('errorId')
 const errorPwField = document.getElementById('errorPassword')
 
 
-function validar(id,p,event){
-    
-    const fieldsNotEmpty = (id !== '' && p !== '')
-    const fieldsEmpty = (id === '' && p === '')
-    const fieldsNotMatching = (id !== dbId && p !== dbPassword)
-    const idErrorMsg = 'Introduce una identificacion'
-    const pwErrorMsg = 'Introduce una contraseña'
-    const validIdMsg = 'Introduce una identificación válida'
-    const validPwMsg = 'Introduce una contraseña válida'
-    const anyFieldEmpty = (id === '' || p === '')
-    const idEmpty = (id === '')
-    const pwEmpty = (p === '')
-    const validId = (id === dbId)
-    const validPw = (p === dbPw)
-    const anyFieldInvalid = (!validId || !validPw)
+function validar(id,p){
 
-    // Validating empty fields
-    if (fieldsEmpty || fieldsNotMatching) {
-        event.preventDefault()
-
-        if (fieldsEmpty || anyFieldEmpty) {
-            if (idEmpty) {
-                errorIdField.innerHTML = idErrorMsg 
-            }
-            if (pwEmpty) {
-                errorPwField.innerHTML = pwErrorMsg
-            }
+    if(id === '' && p === ''){
+        errorId.innerHTML = 'Introduce una identificacion'
+        errorPassword.innerHTML = 'Introduce una contraseña'
+        console.log('Por favor ingresa un dato')
+    }
+    else if(id === ''){
+        errorId.innerHTML = 'Introduce una identificación'
+    }
+    else if(p === ''){
+        errorPassword.innerHTML = 'Introduce una contraseña'
+    }
+    else if(id != dbId){
+        errorId.innerHTML = 'Introduce una identificación válida'
+    }
+    else if(p != dbPassword){
+        errorPassword.innerHTML = 'Introduce una contraseña válida'
+    }
+    else{
+        if(id === dbId && p === dbPassword){
+            window.location.href = './bienvenida.html'
         }
     }
-
-    // Validating not empty fields
-    if (fieldsNotEmpty) {
-
-        // stopping redirect
-        if (anyFieldInvalid) {
-            event.preventDefault()
-        }
-
-        // Adding error messages
-        if (fieldsNotMatching) {
-            if (!validId) {
-                errorIdField.innerHTML = validIdMsg
-            }
-            if (!validPw) {
-                errorPwField.innerHTML = validPwMsg
-            }
-        }
-    }
-
-    // if(id === '' && p === ''){
-    //     errorId.innerHTML = 'Introduce una identificacion'
-    //     errorPassword.innerHTML = 'Introduce una contraseña'
-    //     console.log('Por favor ingresa un dato')
-    // }
-    // else if(id === ''){
-    //     errorId.innerHTML = 'Introduce una identificación'
-    // }
-    // else if(p === ''){
-    //     errorPassword.innerHTML = 'Introduce una contraseña'
-    // }
-    // else if(id != dbId){
-    //     errorId.innerHTML = 'Introduce una identificación válida'
-    // }
-    // else if(p != dbPassword){
-    //     errorPassword.innerHTML = 'Introduce una contraseña válida'
-    // }
-    // else{
-    //     if(id === dbId && p === dbPassword){
-    //         window.location.href = './bienvenida.html'
-    //     }
-    // }
 }
 
 formulario.addEventListener('submit', function(event) {
+    event.preventDefault()
     const usuario = document.getElementById('username').value
     const password = document.getElementById('password').value
     validar(usuario, password, event)
--- Kata_JavaScript/ProyectoFinal/js/operaciones.js
+++ Kata_JavaScript/ProyectoFinal/js/operaciones.js
@@ -1,23 +1,9 @@
-const totalSaldo = document.getElementById('totalSaldo')
-const totalIngreso = document.getElementById('totalIngreso')
-const totalRetiro = document.getElementById('totalRetiro')
-const ingresar = document.getElementById('ingresar')
-const registrar = document.getElementById('registrar')
-const registrarDato = document.getElementById('registrarDato')
-const retiro = 0;
-const ingreso = 0;
-const saldo = 0;
-const operaciones = []
-const monto = 0;
+const ingreso = document.getElementById('ingreso')
 
-function registrarOperaciones(){
-    if(registrarDato === 0){
-        alert('El monto debe ser un valor mayor a cero');
-        console.log('ingresar')
-    }
+localStorage.setItem("Gabriel", ingreso())
+
+
+
+function ingresoSaldo() {
+    localStorage.registrarDato();
 }
-
-registrar.addEventListener('registrar', function(registrarOperaciones) {
-    registrarOperaciones.console.log(registrarDato);
-
-})
