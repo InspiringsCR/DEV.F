b57d0e4f364c1043de99064423cbb29378a68af4
Gabriel Granados Ureña <gabrig1404@gmail.com>
2022-08-30 21:33:31 -0600

    Final del proyecto

--- Kata_JavaScript/ProyectoFinal/js/operaciones.js
+++ Kata_JavaScript/ProyectoFinal/js/operaciones.js
@@ -1,9 +1,39 @@
-const ingreso = document.getElementById('ingreso')
-
-localStorage.setItem("Gabriel", ingreso())
-
-
-
-function ingresoSaldo() {
-    localStorage.registrarDato();
-}
+var saldo = 0;
+ 
+	function showContent(id, e) {
+		document.getElementById("error").style.display='none';
+ 
+		if (e.checked) {
+			document.getElementById(id).style.display = 'block';
+		}else{
+			document.getElementById(id).style.display = 'none';
+		}
+	}
+ 
+	function deposito() {
+		document.getElementById("error").style.display = 'none';
+ 
+		var valor = parseInt(document.getElementsByName("valor1")[0].value);
+		if(!isNaN(valor))
+		{
+			saldo = saldo + valor;
+			document.getElementById("saldo").innerHTML = saldo;
+		}
+	}
+ 
+	function retiro() {
+		document.getElementById("error").style.display = 'none';
+ 
+		var valor = parseInt(document.getElementsByName("valor2")[0].value);
+		if(!isNaN(valor))
+		{
+			if(valor <= saldo)
+			{
+				saldo = saldo - valor;
+				document.getElementById("saldo").innerHTML = saldo;
+			}else{
+				document.getElementById("error").innerHTML = "La cantidad no puede superar el saldo";
+				document.getElementById("error").style.display = 'block';
+			}
+		}
+	}
